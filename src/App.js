00109a74c699778f48a52ac4cdc1f5f353af838c
Benjamin Jones <benjamin.jones@clifton-asset.co.uk>
2021-09-24 10:07:46 +0100

    Adding files to repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
 
-function App() {
+const App = () => {
+  const [numbers, setNumbers] = useState({arr: [], value: ""});
+
+  const AddNumber = (event) => {
+    if (event.code === "Enter") {
+      let temp = numbers.arr
+      temp.push(numbers.value)
+      setNumbers({arr: temp, value: ""})
+      numbers.value = ""
+    }
+  }
+
+  const NumbersList = () => {
+      return numbers.arr.map((number) => <li>{number}</li>)
+  }
+
+  const handleChange = (event) => {
+    setNumbers({arr: numbers.arr, value: event.target.value})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input type="text" name="number" value={numbers.value} onChange={handleChange} onKeyPress={AddNumber} />
+        <button onClick={AddNumber}>Submit</button>
+      </div>
+      <div>
+        <ul>
+          <NumbersList />
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
